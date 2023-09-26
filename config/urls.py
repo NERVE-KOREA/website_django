fd163219c13ea8b126b01c8810a9fbf0efbddd89
homerunbaII <thomas3737@korea.ac.kr>
2023-09-26 22:48:34 +0900

    0926 template dir move

--- config/urls.py
+++ config/urls.py
@@ -4,10 +4,10 @@ from signup.views import main
 from login import views
 
 urlpatterns = [
+    path('', main ),
     path('signup/', include('signup.urls')),
     path('login/', include('login.urls')),
     path('admin/', admin.site.urls),
-    path('', main ),
     path('myprofile/',include('myprofile.urls')),
     # 다른 앱의 URL 패턴들을 여기에 추가할 수 있습니다.
     # 루트 경로에 대한 URL 패턴을 정의해야 합니다.
