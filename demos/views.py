879df8fe1a56db4677ffb93da02640855de9a615
SSunwo <aidnsunwoo@naver.com>
2023-09-26 12:20:33 +0900

    #12 "feat:html/css static연결 및 페이지 업데이트

--- demos/views.py
+++ demos/views.py
@@ -1,1 +1,15 @@
 from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+
+from django.contrib import admin
+from django.urls import path
+
+import login.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', login.views.index, name='index'),
+]
