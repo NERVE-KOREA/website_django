691e1b7a03f6271d25394ba75b2b867a32ab5626
선우 <125359537+SSunwo@users.noreply.github.com>
2023-10-05 06:03:36 +0900

    #15: 로그인 회원가입 페이지 (#16)

--- config/urls.py
+++ config/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('product/', include('product.urls')),
     # 다른 앱의 URL 패턴들을 여기에 추가할 수 있습니다.
     # 루트 경로에 대한 URL 패턴을 정의해야 합니다.
+
 ]
 
--- static/js/index.js
+++ static/js/index.js
@@ -15,3 +15,21 @@ window.addEventListener('scroll', () => {
 	const bwLeft = window.scrollX;
 	document.querySelector('nav').style.transform = `translateX(-${bwLeft}px)`;
   });
+
+
+// 자동롤링배너
+
+let roller = document.querySelector('.banner1');
+roller.id = 'roller1'; // 아이디 부여
+
+let clone = roller.cloneNode(true)
+// cloneNode : 노드 복제. 기본값은 false. 자식 노드까지 복제를 원하면 true 사용
+clone.id = 'roller2';
+document.querySelector('.banner-wrap').appendChild(clone); // wrap 하위 자식으로 부착
+
+document.querySelector('#roller1').style.left = '0px';
+document.querySelector('#roller2').style.left = document.querySelector('.flow-text').offsetWidth + 'px';
+// offsetWidth : 요소의 크기 확인(margin을 제외한 padding값, border값까지 계산한 값)
+
+roller.classList.add('original');
+clone.classList.add('clone');
