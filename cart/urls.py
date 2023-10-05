170219f72c6a64b27fdcff3a798594509f9d9f63
SSunwo <aidnsunwoo@naver.com>
2023-10-05 20:41:58 +0900

    자잘한거 수정

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     # 다른 URL 패턴들...
-    path('', views.cart_view, name='cart_view'),
+    path('', views.cart_view, name='cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def cart_view(request):
     user = request.user
-
     return render(request, "cart.html")
