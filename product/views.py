f6906229072b6cbe392ef1d51c71f62f75c1030a
homerunbaII <thomas3737@korea.ac.kr>
2023-10-09 16:00:42 +0900

    cartview

--- product/views.py
+++ product/views.py
@@ -1,8 +1,21 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-
+from cart.models import Cart
 
 def redpill_view(request):
+    user = request.user
+    user_id = user.user_id
+    if request.method == 'POST':
+        redpill_quantity = request.POST['redpill_quantity']
+        try:
+            cart = Cart.objects.filter(user_id = user_id)
+        except Cart.DoesNotExist:
+            new_cart = Cart(user_id = user_id, redpill_quantity = redpill_quantity)
+            new_cart.save()
+            pass
+        else:
+            cart.redpill_quantity += redpill_quantity
+            cart.save()
     return render(request, "redpill.html") 
 
 def bluepill_view(request):
