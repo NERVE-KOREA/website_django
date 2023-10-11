56f680291c63d3dda84bc8c6354765c8c8574ad6
homerunbaII <thomas3737@korea.ac.kr>
2023-10-11 15:49:57 +0900

    cart feature add

--- cart/views.py
+++ cart/views.py
@@ -7,33 +7,41 @@ from django.shortcuts import get_object_or_404
 @login_required
 def cart_view(request):
     user = request.user
-    return render(request, "cart.html")
+    user_id = user.user_id
+    try:
+        cart = Cart.objects.get(user_id = user_id)
+        redpill_quantity = cart.redpill_quantity
+        bluepill_quantity = cart.bluepill_quantity
+    except Cart.DoesNotExist:
+        new_cart = Cart.objects.create(user_id = user_id, redpill_quantity = 0, bluepill_quantity = 0)
+        new_cart.save()
+        cart = Cart.objects.get(user_id = user_id)
+        redpill_quantity = cart.redpill_quantity
+        bluepill_quantity = cart.bluepill_quantity
+    if request.method == 'POST':
+        redpill_quantity = request.POST.get('redpill_quantity')
+        bluepill_quantity = request.POST.get('bluepill_quantity')
+        cart = Cart.objects.get(user_id = user_id)
+        cart.redpill_quantity = redpill_quantity
+        cart.bluepill_quantity = bluepill_quantity
+        cart.save()
+        return redirect('order')
+    return render(request, "cart.html",{'redpill_quantity': redpill_quantity, 'bluepill_quantity': bluepill_quantity})
 
 @login_required
 def order_view(request):
     user = request.user
     return render(request, "order.html")
 
-@login_required
-def product_add_to_cart_redpill(request):
-    user = request.user
-    user_id = user.user_id
-    try:
-        cart = Cart.objects.get(user_id=user_id)
-        # 카트가 이미 있는 경우, 품목 개수를 +1 업데이트
-        print(cart.cart_id)
-        cart['redpill_quantity'] += 1
-        cart.save()
-    except Cart.DoesNotExist:
-        # 카트가 없는 경우, 새로운 카트 생성 후 품목 추가
-        cart = Cart.objects.create(user_id=user_id, redpill_quantity = user.repill_quantity)
+
+
     # 나머지 품목 추가 로직 작성
-    return redirect('cart.html')  # 장바구니 페이지로 리다이렉션
+    # 장바구니 페이지로 리다이렉션
         
 
-# def add_cart(request,cart_id):
-#     product =  Cart.objects.get(product_id=product_id(request))
-#     cart_item = Cart_Detail.objects.get()
+# def add_cart(request,product_id):
+#     product = Product.objects.get(id=product_id)
+#     cart_item = Orders_Detail.objects.get()
 
 
 
